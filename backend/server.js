2555f6427f0be75d4d259ce6c678455b12c64ab4
Kizzy Sinar <Ranisimo@users.noreply.github.com>
2021-11-24 05:11:48 +0000

    removed heartbeat/ ping&pong function couldn't set it up in time on the client side removed it for sake of readability

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,3 @@
-const { WSAEWOULDBLOCK } = require('constants');
 const express = require('express');
 const { setInterval } = require('timers/promises');
 const app = express();
@@ -6,33 +5,14 @@ const server = require('http').createServer(app);
 const WebSocket = require('ws');
 const lib = require('./modules/initialiser');
 
-function heartbeat() {
-  this.isAlive = true;
-}
-
-wss = new WebSocket.Server({ server:server });
+wss = new WebSocket.Server({ server:server }); // Global wss
 
 wss.on('connection', function connection(ws) {
-  ws.isAlive = true;
-  ws.on('pong', heartbeat);
   ws.on('message', message => {
     console.log(`Received message => ${message}`);
     lib.init();
-
-  })
-  ws.on('update', message => {
-    ws.send('Updated');
   })
 });
-
-const interval = setInterval(function ping() {
-  wss.clients.forEach(function each(ws) {
-    if (ws.isAlive === false) return ws.terminate();
-
-    ws.isAlive = false;
-    ws.ping();
-  });
-}, 30000);
 
 wss.on('close', function close() {
   clearInterval(interval);
